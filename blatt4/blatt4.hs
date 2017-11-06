d48135dd48e663e31d46ce2ef7a55c487b47d33c
WolfgangBischoff <32836649+WolfgangBischoff@users.noreply.github.com>
2017-11-06 21:26:31 +0100

    Update blatt4.hs

--- blatt4/blatt4.hs
+++ blatt4/blatt4.hs
@@ -1,9 +1,18 @@
 -- checking whether the given tree is a search tree
-import Data.Tree
 
 data BTree a = Empty | Node a (BTree a) (BTree a)
  deriving (Eq, Show, Read)
---isSorted :: Ord a => BTree a -> Bool
 
+tree = Node 2 (Node 1 Empty Empty) (Node 4 (Node 3 Empty Empty)(Node 5 Empty Empty))
+treeFail = Node 5 (Node 1 Empty Empty) (Node 4 (Node 3 Empty Empty)(Node 1 Empty Empty))
 
+isSorted :: Ord a => BTree a -> Bool
+isSorted a = isAscending list
+ where list = getList a
 
+getList Empty = []
+getList (Node a l r) = getList l ++ [a] ++ getList r
+
+isAscending :: Ord a => [a] -> Bool
+isAscending [x] = True
+isAscending (x:xs) = x <= head (xs) && isAscending xs
